272277bcc9805e567a6d6e92524b9e19c17c7095
ilyaTT <ilya.tt07@gmail.com>
2020-04-28 14:18:12 +0300

    описание резюме

--- python/django-app/main/authentication.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from rest_framework.authentication import SessionAuthentication
-
-
-class NoCsrfSessionAuthentication(SessionAuthentication):
-    def enforce_csrf(self, request):
-        return
